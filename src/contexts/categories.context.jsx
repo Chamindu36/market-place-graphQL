b061cf285d0fbb41083db683b78808289db9c95b
Chamindu <cudakara@gmail.com>
2023-08-30 18:25:37 +0530

    Use categories graphQL query to fetch data

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
@@ -6,19 +7,41 @@ export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+// define graphQL query
+const CollectionsQuery = gql`
+  query GetCollections {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  // use useQuery hook to fetch data from graphQL server
+  const { loading, error, data } = useQuery(CollectionsQuery);
+
   const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection.items;
+        return accumulator;
+      }, {});
+
+      setCategoriesMap(collectionsMap);
     };
+  }, [data]);
 
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading, error };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
